31c254eb76c95a43d7930735fcc59930b3022356
Prajakta Petkar <praj.petkar2529@gmail.com>
2026-06-22 13:21:54 +0200

    Add Swagger correction

--- EmployeeApi/Program.cs
+++ EmployeeApi/Program.cs
@@ -2,15 +2,20 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+//builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
+app.UseSwagger();
+app.UseSwaggerUI();
+
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
+//if (app.Environment.IsDevelopment())
+//{
+//   app.MapOpenApi();
+//}
 
 app.UseHttpsRedirection();
 
